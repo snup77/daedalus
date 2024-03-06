9159cc60c9e284bab3e31c7b89047e76efbc2efa
Anup Sheth <anup.sheth@gmail.com>
2024-03-05 23:57:07 -0500

    homepage practice styles

--- wp-content/themes/daedalus/assets/js/sticky.js
+++ wp-content/themes/daedalus/assets/js/sticky.js
@@ -1,6 +1,5 @@
 const header = document.querySelector(".page-header");
 const toggleClass = "sticky-styles";
-console.log(header)
 window.addEventListener("scroll", () => {
   const currentScroll = window.scrollY;
   if (currentScroll > 75) {
