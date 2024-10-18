a9b9a6124ae9a37fc44c43405161b5f745f7c06b
Anup Sheth <anup.sheth@gmail.com>
2024-10-18 02:13:01 -0400

    add footer and page template

---
+++ wp-content/themes/daedalus/patterns/header-transparent2.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: Header Transparent2
+ * Slug: daedalus/header-transparent2
+ * Categories: header
+ */
+?>
+
+<!-- wp:group {"align":"wide","className":"page-header","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide page-header"><!-- wp:group {"align":"wide","className":"desktop-only","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide desktop-only"><!-- wp:group {"layout":{"type":"constrained","contentSize":"1280px","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"header-button-icon-positioning header-button-background","style":{"color":{"background":"#f08749"},"spacing":{"padding":{"left":"40px","right":"40px"}},"border":{"radius":"0px","width":"0px","style":"none"}}} -->
+<div class="wp-block-button header-button-icon-positioning header-button-background"><a class="wp-block-button__link has-background wp-element-button" href="http://susan-curcio-law.local/contact/" style="border-style:none;border-width:0px;border-radius:0px;background-color:#f08749;padding-right:40px;padding-left:40px"><img class="wp-image-131" style="width: 15px;" src="http://susan-curcio-law.local/wp-content/themes/daedalus/assets/images/compass.svg" alt="">REQUEST A CONSULTATION</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"8px","bottom":"8px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:8px;padding-bottom:8px"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":250,"shouldSyncIcon":false,"className":"logo-placement-desktop"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":15,"textColor":"base-2","icon":"menu","customOverlayBackgroundColor":"#63605f","className":"navigation-link-styles","style":{"spacing":{"margin":{"top":"0"},"blockGap":"var:preset|spacing|20"},"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small","layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
