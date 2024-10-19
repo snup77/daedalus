fcdf73541868c23b3cfe7cb3073d67a5319b2101
Anup Sheth <anup.sheth@gmail.com>
2024-10-19 00:42:44 -0400

    new contact form pattern

---
+++ wp-content/themes/daedalus/patterns/contact-form-2-col.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Title: Contact Form Section 2
+ * Slug: daedalus/contact-form-2-col
+ * Categories: Contact Forms
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"base-2","layout":{"type":"constrained","contentSize":"1280px","justifyContent":"center"}} -->
+<div class="wp-block-group has-base-2-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|30"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"1.75rem"},"spacing":{"padding":{"bottom":"var:preset|spacing|10"},"margin":{"right":"0","left":"0"}}}} -->
+<h2 class="wp-block-heading" style="margin-right:0;margin-left:0;padding-bottom:var(--wp--preset--spacing--10);font-size:1.75rem;font-style:normal;font-weight:600">Location</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"right":"0","left":"0"}}}} -->
+<p style="margin-right:0;margin-left:0">225 Sandoval Street</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"right":"0","left":"0"}}}} -->
+<p style="margin-right:0;margin-left:0">Santa Fe, NM 87501</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"1.75rem"},"spacing":{"padding":{"bottom":"var:preset|spacing|10"},"margin":{"right":"0","left":"0"}}}} -->
+<h2 class="wp-block-heading" style="margin-right:0;margin-left:0;padding-bottom:var(--wp--preset--spacing--10);font-size:1.75rem;font-style:normal;font-weight:600">Contact</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"right":"0","left":"0"}}}} -->
+<p style="margin-right:0;margin-left:0">P: (505) 264-0912</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"right":"0","left":"0"}}}} -->
+<p style="margin-right:0;margin-left:0">E: hello@susancurciolaw.com</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%","className":"contact-page-form-container"} -->
+<div class="wp-block-column contact-page-form-container" style="flex-basis:66.66%"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"1.75rem"},"spacing":{"padding":{"bottom":"var:preset|spacing|10"}}}} -->
+<h2 class="wp-block-heading" style="padding-bottom:var(--wp--preset--spacing--10);font-size:1.75rem;font-style:normal;font-weight:600">How can we help you?</h2>
+<!-- /wp:heading -->
+
+<!-- wp:shortcode -->
+[contact-form-7 id="bd03c56" title="Contact form 1"]
+<!-- /wp:shortcode --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
