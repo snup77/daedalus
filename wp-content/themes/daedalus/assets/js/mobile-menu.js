ea011dfec582856b82c79180028d975440179752
Anup Sheth <anup.sheth@gmail.com>
2024-10-23 13:54:45 -0400

    add mobile menu listeners

--- wp-content/themes/daedalus/assets/js/mobile-menu.js
+++ wp-content/themes/daedalus/assets/js/mobile-menu.js
@@ -24,24 +24,41 @@ opacityLayer.addEventListener("click", function() {
     opacityLayer.classList.remove("modal-background");
 });
 
+
+// get link and button elements
+const navLink = document.querySelector(".wp-block-navigation-submenu a");
+
 const chevrons = document.getElementsByClassName("wp-block-navigation__submenu-icon");
 const chevron = chevrons[0];
 
-const navLink = document.querySelector(".wp-block-navigation-submenu a");
 
-
-
+// listen for click event on submenu link and open-close menu
 navLink.addEventListener("click", function(e) {
     e.preventDefault();
 
     let chevronAttribute = chevron.getAttribute("aria-expanded");
-    console.log(chevronAttribute);
 
-    chevron.setAttribute("aria-expanded", "true");
-
-    // if (chevronAttribute !== false) {
-    //     chevron.setAttribute("aria-expanded", "false");
-    // } else {
-    //     chevron.setAttribute("aria-expanded", "true");
-    // }
+    if (chevronAttribute === "false") {
+        chevron.setAttribute("aria-expanded", "true");
+    }
+    
+    if (chevronAttribute === "true") {
+        chevron.setAttribute("aria-expanded", "false");
+    }
 });
+
+// listen for click event on chevron and open-close menu. added because of bug; not sure why this fixes the bug but it does :)
+chevron.addEventListener("click", function(e) {
+    e.stopPropagation();
+
+    let chevronAttribute2 = chevron.getAttribute("aria-expanded");
+
+    if (chevronAttribute2 === "false") {
+        chevron.setAttribute("aria-expanded", "true");
+    }
+    
+    if (chevronAttribute2 === "true") {
+        chevron.setAttribute("aria-expanded", "false");
+    }
+}, true);
+
